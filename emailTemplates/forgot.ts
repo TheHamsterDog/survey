7457ca6e2e32e76c4fe8a6848f8ecfd883e58fa2
Shreshth Verma <shreshth@shreshthverma.me>
2022-01-14 17:04:48 +0530

    feat: fixed email glitch

--- emailTemplates/forgot.ts
+++ emailTemplates/forgot.ts
@@ -1,7 +1,7 @@
 const Token= (token)=>{return(`<html ><style type="text/css">	.button {  background-color: #4CAF50; /* Green */  border: none;  color: white;  padding: 15px 32px;  text-align: center;  text-decoration: none;  display: inline-block;  font-size: 16px;}h1{	padding-top: 100px;	color:Green;}body{	text-allign:center;}</style><body>
 <center>
     <h1>Click on the Button to reset your password</h1>
-    <a color='white' bg-color='#4CAF50' href=${'http://localhost:5000/api/auth/forgot/'+token}>
+    <a color='white' bg-color='#4CAF50' href=${'https://survey.shreshthverma.me/api/auth/forgot/'+token}>
 <button class='button' bg-color='#4CAF50' width='100%'>Click Here </button></a>
 </center>
 </body></html>`)}
--- emailTemplates/verify.ts
+++ emailTemplates/verify.ts
@@ -4,7 +4,7 @@
 const Token= (token)=>{return(`<html ><style type="text/css">	.button {  background-color: #4CAF50; /* Green */  border: none;  color: white;  padding: 15px 32px;  text-align: center;  text-decoration: none;  display: inline-block;  font-size: 16px;}h1{	padding-top: 100px;	color:Green;}body{	text-allign:center;}</style><body>
 	<center>
 		<h1>Click on the Button to Verify Your Surveys Account</h1>
-	<a class='button' bg-color='#4CAF50' href=${'http://localhost:5000/api/auth/confirmation/'+token}>Click Here </a>
+	<a class='button' bg-color='#4CAF50' href=${'https://survey.shreshthverma.me/api/auth/confirmation/'+token}>Click Here </a>
 </center>
 </body></html>`)}
 export default Token;
